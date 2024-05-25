0a89dfadeeaa2f26878dd439e88d2c2f14b54668
championLuo <491936385@qq.com>
2024-05-25 17:51:00 +0800

    01- 解析启动cargo run -- csv... 命令行参数， 并打印出参数列表

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,37 @@
+//rcli csv -i input.csv -o output.json --header -d ','
+
+use clap::{Parser, Subcommand};
+
+#[derive(Debug, Parser)] //相当于注解，打上标识的结构体再特定场景下会有特别处理
+#[command{name="rcli", version, author, about, long_about = None}]
+struct Opts {
+    #[command(subcommand)]
+    cmd: SubCommand,
+}
+
+#[derive(Subcommand, Debug)] // Opts中实现了Debug trait， 所以类型属性中也应该实现Debug， 不然无法进行debug输出
+enum SubCommand {
+    #[command(name = "csv", about = "Show Csv, or Convert CSV to other formats")]
+    Csv(CsvOpts),
+}
+
+#[derive(Debug, Parser)]
+struct CsvOpts {
+    #[arg(short, long)]
+    input: String,
+
+    #[arg(short, long, default_value = "output.json")]
+    output: String,
+
+    #[arg(short, long, default_value_t = ',')]
+    delimiter: char,
+
+    #[arg(long, default_value_t = false)]
+    header: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
-    println!("Hello, world!");
+    // println!("Hello, world!");
+    let opts = Opts::parse();
+    println!("{:?}", opts);
 }
