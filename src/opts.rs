c252f0f90f3a56ae8560be740136c14fbf235dff
championLuo <491936385@qq.com>
2024-06-04 00:25:04 +0800

    01-8-02 支持转换成yaml和toml(暂时无法实现)- 第一步-新增format参数

--- src/opts.rs
+++ src/opts.rs
@@ -1,9 +1,19 @@
+// use anyhow::{Error, Ok};
 use clap::{Parser, Subcommand};
-use std::path::Path;
+// use serde_json::from_str;
+// use std::{any, path::Path, process::Output, str::FromStr};
+use std::{path::Path, str::FromStr};
+
+#[derive(Debug, Clone, Copy)]
+pub enum OutputFormat {
+    Json,
+    Yaml,
+    Toml,
+}
 
 #[derive(Debug, Parser)]
 pub struct CsvOpts {
-    #[arg(short, long, value_parser= valid_input_path)]
+    #[arg(short, long, value_parser = valid_input_path)]
     pub input: String,
 
     #[arg(short, long, default_value = "output.json")]
@@ -14,6 +24,67 @@ pub struct CsvOpts {
 
     #[arg(long, default_value_t = false)]
     header: bool,
+
+    #[arg(short, long, value_parser = parse_format, default_value = "json")]
+    pub format: OutputFormat,
+}
+
+// fn parse_format(format: &str) -> Result<OutputFormat, &'static str> {
+// match format.to_lowercase().as_str() {
+//     "json" => Ok(OutputFormat::JSON),
+//     "yaml" => Ok(OutputFormat::YAML),
+//     "toml" => Ok(OutputFormat::TOML),
+//     _ => Err("Invalid format"), //注意match的写法， 最后的default 使用 _ => Err("Invalid format")
+// }
+
+// }
+
+//parse_format 方法改成调用FromStr的trait
+fn parse_format(format: &str) -> Result<OutputFormat, anyhow::Error> {
+    //<OutputFormat>() 可以省略， 会自动推导类型
+    // format.parse::<OutputFormat>()
+    format.parse()
+}
+
+//其他类型转成字符串
+impl From<OutputFormat> for &'static str {
+    fn from(format: OutputFormat) -> Self {
+        match format {
+            OutputFormat::Json => "json",
+            OutputFormat::Yaml => "yaml",
+            OutputFormat::Toml => "toml",
+        }
+    }
+}
+
+//字符串转其他类型
+// impl TryFrom<&str> for OutputFormat {
+//     type Error = anyhow::Error;
+//     // type Error = &'static str;
+
+//     fn try_from(value: &str) -> Result<Self, Self::Error> {
+//         match value.to_lowercase().as_str() {
+//             "json" => Ok(OutputFormat::JSON),
+//             "yaml" => Ok(OutputFormat::YAML),
+//             "toml" => Ok(OutputFormat::TOML),
+//             // _ => Err("Invalid format"),
+//             v => anyhow::bail!("Unsupported format: {}", v),
+//         }
+//     }
+// }
+
+// impl TryFrom<&str> 如果要用Parser的话，最好改写成FromStr的trait
+impl FromStr for OutputFormat {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s.to_lowercase().as_str() {
+            "json" => Ok(OutputFormat::Json),
+            "yaml" => Ok(OutputFormat::Yaml),
+            "toml" => Ok(OutputFormat::Toml),
+            v => anyhow::bail!("Unsupported format: {}", v),
+        }
+    }
 }
 
 #[derive(Debug, Parser)] //相当于注解，打上标识的结构体再特定场景下会有特别处理
--- src/process.rs
+++ src/process.rs
@@ -19,28 +19,19 @@ struct Player {
     kit: u8,
 }
 
-// #[command(name="csv_proc")]
 pub fn process_csv(input: &str, output: &str) -> Result<()> {
     let mut reader = Reader::from_path(input)?;
-    let mut ret_vec = Vec::with_capacity(128); // let records = reader.deserialize()
-                                               // .map(|record| record?)>
-                                               // .collect::<Vec<Player>>();
-                                               //这里borrow了reader 2个mutable reference
-                                               //通过使用clone() 来消除重复可变引用的影响
+    let mut ret_vec = Vec::with_capacity(128);
     let headers = reader.headers()?.clone();
     // for result in reader.deserialize() {
     for result in reader.records() {
         let record = result?;
         // println!("{:?}", record);
+        //zip的作用是将两个迭代器的元素组合成一个新的迭代器【header， record】
+        //collect::<Value>()-> 将元组的迭代器转换为json对象
         let json_value = headers.iter().zip(record.iter()).collect::<Value>();
         ret_vec.push(json_value);
     }
-    // for result in reader.deserialize() {
-    //     // Notice that we need to provide a type hint for automatic
-    //     // deserialization.
-    //     let record: Player = result?;
-    //     println!("{:?}", record);
-    // }
     let to_string_pretty = serde_json::to_string_pretty(&ret_vec)?;
     fs::write(output, to_string_pretty)?;
     Ok(())
